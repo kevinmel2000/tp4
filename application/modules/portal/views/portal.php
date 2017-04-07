1cd52ab78c9a581a72ab1daf7a861f73e83c4a27
mrapry <mr.apry.prastowo@gmail.com>
2017-04-07 10:05:04 +0700

    Portal

--- application/modules/portal/views/portal.php
+++ application/modules/portal/views/portal.php
@@ -93,8 +93,46 @@
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <div class="panel panel-default">
+    <div class="col-md-3">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-body panel-body-image">
+                    <img src="<?php echo base_url()?>assets/assets/images/ocean.jpg" alt="Ocean"/>
+                    <a href="#" class="panel-body-inform">
+                        <span class="fa fa-facebook"></span>
+                    </a>
+                </div>
+                <div class="panel-body">
+                    <h3>Ocean &mdash; body of saline water</h3>
+                    <p>On Earth, an ocean is one of the major conventional divisions of the World Ocean, which occupies two-thirds of the planet's surface.</p>
+                </div>
+                <div class="panel-footer text-muted">
+                    <span class="fa fa-clock-o"></span> 3d ago &nbsp;&nbsp;&nbsp;
+                    <span class="fa fa-comment-o"></span> 36
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-body panel-body-image">
+                    <img src="<?php echo base_url()?>assets/assets/images/ocean.jpg" alt="Ocean"/>
+                    <a href="#" class="panel-body-inform">
+                        <span class="fa fa-facebook"></span>
+                    </a>
+                </div>
+                <div class="panel-body">
+                    <h3>Ocean &mdash; body of saline water</h3>
+                    <p>On Earth, an ocean is one of the major conventional divisions of the World Ocean, which occupies two-thirds of the planet's surface.</p>
+                </div>
+                <div class="panel-footer text-muted">
+                    <span class="fa fa-clock-o"></span> 3d ago &nbsp;&nbsp;&nbsp;
+                    <span class="fa fa-facebook"></span> 36
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="panel panel-default" style="height: 660px">
             <div class="panel-heading">
                 <div class="panel-title-box">
                     <h3>Kemitraan</h3>
