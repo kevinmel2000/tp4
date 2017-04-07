296a77d51a0d11b2eb37bda5a4803077f2264fc3
Filza Arianti <filzaarianti@Filzas-MacBook-Pro.local>
2017-04-07 11:38:25 +0700

    Local commit

---
+++ application/modules/laporan/views/laporan.php
@@ -0,0 +1,57 @@
+<?php 
+/**
+ * Created by Sublime.
+ * User: filza
+ * Date: 7/04/2017
+ */
+?>
+
+<br>
+<div class="row">
+	<div class="col-md-9">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel Title 1</h3>
+			</div>
+			<div class="panel-body">
+
+				<div class="col-md-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Panel Title 1</h3>
+						</div>
+						<div class="panel-body">
+
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="tempatiklan" style="border:ridge;">
+						<img src="<?php echo base_url() ?>/assets/img/IKLAN.jpg" width="275" height= "75">
+						<p>ini buronan</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel Title 1</h3>
+			</div>
+			<div class="panel-body">
+				<img src="<?php echo base_url() ?>/assets/img/IKLAN.jpg" width="275" height= "75">
+				<hr>
+				<img src="<?php echo base_url() ?>/assets/img/IKLAN2.jpg" width="275" height= "75">
+				<hr>
+				<img src="<?php echo base_url() ?>/assets/img/IKLAN3.jpg" width="275" height= "75">
+				<hr>
+				<img src="<?php echo base_url() ?>/assets/img/IKLAN4.jpg" width="275" height= "75">
+				<hr>
+			</div>
+		</div>
+	</div>
+</div>
